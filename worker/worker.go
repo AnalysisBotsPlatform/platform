85d9b3fe9646793e48ddfaf46d3449e7d7da1ddf
Merlin Köhler <merlin-koehler@t-online.de>
2016-02-12 14:36:54 +0100

    Added comments to the worker functions

--- worker/worker.go
+++ worker/worker.go
@@ -47,6 +47,9 @@ var pauseChan chan bool
 var runningTasks map[int64]chan bool
 
 // Initialization of the worker. Sets up the RPC infrastructure.
+// Furthermore here the runners for ScheduledTask and OneTimeTask are being
+// spawned in case there exists some entries in the database for those that
+// should be execued.
 func Init(port, cache_path string) error {
 	api = NewWorkerAPI()
 	rpc.Register(api)
@@ -107,12 +110,24 @@ func CreateNewTask(parentTaskId int64) (int64, error) {
 	return newTask.Id, nil
 }
 
+// Creates a new go routine, which handles the execution of the scheduled task
+// according to the specified dates.
+// First it creates a new channel in order to cancel the created go routine and
+// then it inserts this into a map in order to be able to address this
+// particular channel later on.
+// In the end it runs the go routine (asynchronous call).
 func RunScheduledTask(stid int64) {
 	cancelChan := make(chan bool, 1)
 	runningTasks[stid] = cancelChan
 	go runScheduledTask(stid, cancelChan)
 }
 
+// Creates a new go routine, which handles the execution of the one time task
+// according to the specified date.
+// First it creates a new channel in order to cancel the created go routine and
+// then it inserts this into a map in order to be able to address this
+// particular channel later on.
+// In the end it runs the go routine (asynchronous call).
 func RunOneTimeTask(otid int64) {
 	cancelChan := make(chan bool)
 	runningTasks[otid] = cancelChan
@@ -124,6 +139,14 @@ func RunOneTimeTask(otid int64) {
 // cancel GroupTasks
 // ############################
 
+// Cancels the scheduling go routine for this particular task and its "child"
+// tasks that are being executed at the moment by some worker.
+// It first sends a value on the corresponding cancel channel which causes the
+// go routine to terminate and then updates the status of this bot to 
+// "Complete".
+// Then it retrieves all the "child" tasks (the actual executions) of this task
+// that are still running (being executed by some worker), iterates over them
+// and by that cancels the execution of all of them.
 func CancelScheduledTask(stid int64) error {
 	cancelChan, ok := runningTasks[stid]
 	if ok {
@@ -145,6 +168,14 @@ func CancelScheduledTask(stid int64) error {
 	return err
 }
 
+// Cancels the scheduling go routine for this particular task and its "child"
+// tasks that are being executed at the moment by some worker.
+// It first sends a value on the corresponding cancel channel which causes the
+// go routine to terminate and then updates the status of this bot to
+// "Complete".
+// Then it retrieves all the "child" tasks (the actual executions) of this task
+// that are still running (being executed by some worker), iterates over them
+// and by that cancels the execution of all of them.
 func CancelOneTimeTask(stid int64) error {
 	cancelChan, ok := runningTasks[stid]
 	if ok {
@@ -166,6 +197,11 @@ func CancelOneTimeTask(stid int64) error {
 	return err
 }
 
+// Cancels all "child" tasks of the particular event task.
+// Therefore it first sets the status of the corresponding event task in the
+// database to complete.
+// Then it retrieves all the currently executed tasks from the databse, iterates
+// over them and cancels them.
 func CancelEventTask(stid int64) error {
 	err := db.UpdateEventTaskStatus(stid, db.Complete)
 	runningChildren, gErr := db.GetActiveChildren(stid)
@@ -180,6 +216,9 @@ func CancelEventTask(stid int64) error {
 	return err
 }
 
+// Cancels the actually execution of that instant task.
+// Therefore it retrieves the "child" task (there should not be more than one)
+// for that task and cancels it.
 func CancelInstantTask(stid int64) error {
 	runningChildren, gErr := db.GetActiveChildren(stid)
 	if gErr != nil {
@@ -214,10 +253,26 @@ func CancelTimedOverTasks() {
 	}
 }
 
+// Cancels all schedulers that are responsible for scheduling the registered
+// event tasks and one time tasks just before the shutdown of the system.
+// All of the schedulers are listening to the pauseChannel. Closing that channel
+// triggers a broadcast on that channel and then closes it.
+// Therefore all running scheduler listening on that channel will receive a
+// value and then terminate.
 func StopPeriodRunners() {
 	close(pauseChan)
 }
 
+// Takes care of the execution of the task according to the specified dates
+// (identified by the cron expression). It is executing an infinite loop in
+// which it is managing the scheduling.
+// In the loop it first computes the time to sleep until the next execution and
+// updates this time in the database.
+// Then it sleeps for the calculated amount of time and after that it creates
+// a new task.
+// In parallel to the sleeping it listens to the channel to cancel the task and
+// terminate and to the one to terminate temporarily but do not mark it as
+// canceled in the database.
 func runScheduledTask(stid int64, cancelChan chan bool) {
 	for {
 		scheduledTask, err := db.GetScheduledTask(stid)
@@ -245,6 +300,12 @@ func runScheduledTask(stid int64, cancelChan chan bool) {
 	}
 }
 
+// Takes care of the execution of the task according to the specified date.
+// It computes the time to sleep, sleeps for that amount of time and after the
+// time has expired it executes the task and terminates.
+// While sleeping it also listens to the cancel channel in order to terminate
+// after the cancellation of the task and on the pause channel in order to
+// terminate temporarily (before a system shutdown).
 func runOneTimeTask(otid int64, cancelChan chan bool) {
 	oneTimeTask, err := db.GetOneTimeTask(otid)
 	if err != nil {
@@ -263,6 +324,8 @@ func runOneTimeTask(otid int64, cancelChan chan bool) {
 	}
 }
 
+// Retrieves the active scheduled and event tasks and starts a new go routine
+// to schedule them.
 func recoverActiveTasks() {
 	sched_ids, err := db.GetScheduledTaskIdsWithStatus(db.Active)
 	if err == nil {
